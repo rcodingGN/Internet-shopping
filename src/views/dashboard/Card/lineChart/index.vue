062a96d73fe886416c0244b0820c154e976155ab
rcodingGN <TrumpGN@163.com>
2022-12-22 16:53:59 +0800

    2022.12.22 16:54

---
+++ src/views/dashboard/Card/lineChart/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="charts" ref="charts"></div>
+</template>
+
+<script>
+// 引入echarts
+import*as echarts from 'echarts';
+
+export default {
+    name:'lineChart',
+    mounted(){
+        // 初始化linechart实例
+        let lineCharts = echarts.init(this.$refs.charts);
+        // 配置数据
+        lineCharts.setOption({
+            xAxis:{},
+            yAxis:{},
+            // 系列
+            series:[
+                {
+                    
+                }
+            ]
+        })
+    }
+
+}
+</script>
+
+<style scoped>
+.charts{
+    height: 100%;
+    width: 100%;
+}
+</style>
